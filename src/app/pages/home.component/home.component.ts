a9fc3efb32ec0660909f8efeb80d7da101f06cbf
Gael Duran <ldt76578@lasallebajio.edu.mx>
2025-11-28 22:11:18 -0600

    fix: refactor map initialization in HomeComponent; streamline map setup and enhance marker loading logic

--- src/app/pages/home.component/home.component.ts
+++ src/app/pages/home.component/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { PlaceService } from '../../core/services/place.service';
 import { Place } from '../../core/models/place.model';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import * as mapboxgl from 'mapbox-gl';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../../environments/environment';
 
@@ -21,64 +20,30 @@ export class HomeComponent implements AfterViewInit {
   constructor(private placeService: PlaceService) {}
 
   ngAfterViewInit(): void {
-    this.initMap();
-    this.loadPlaces();
-    setTimeout(() => this.map?.resize(), 200);
+    setTimeout(() => {
+      this.initMap();
+      this.loadPlaces();
+    }, 0);
   }
 
   initMap(): void {
-    mapboxgl.accessToken = environment.MAPBOX_TOKEN;
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-102.5528, 23.6345],
       zoom: 5,
-      interactive: false
-      
+      accessToken: environment.MAPBOX_TOKEN as any
     });
-
-    const canvas = this.map.getCanvas();
-    if (canvas) {
-      canvas.style.pointerEvents = 'none';
-    }
-
-    this.map.on('load', () => {
-      // reactivar pointer events y forzar resize
-      if (canvas) {
-        canvas.style.pointerEvents = '';
-      }
-
-      try {
-        this.map.scrollZoom.enable();
-        this.map.boxZoom.enable();
-        this.map.dragRotate?.enable?.();
-        this.map.dragPan.enable();
-        this.map.doubleClickZoom.enable();
-        this.map.touchZoomRotate.enable();
-      } catch (e) {
-        console.warn('No se pudieron activar todas las interacciones', e);
-      }
-
-      this.map.resize();
-      if (this.places.length && this.map.isStyleLoaded && this.map.isStyleLoaded()) {
-        this.addMarkers();
-      }
-    });
-
-    this.map.on('error', (evt) => console.error('Mapbox error:', evt));
   }
-  
+
   loadPlaces(): void {
     this.placeService.getPlaces().subscribe(data => {
       this.places = data;
-      if (this.map && this.map.isStyleLoaded && this.map.isStyleLoaded()) {
-        this.addMarkers();
-      }
+      this.addMarkers();
     });
   }
 
   addMarkers(): void {
-     if (!this.map) return;
     this.places.forEach(place => {
       new mapboxgl.Marker()
         .setLngLat([place.longitude, place.latitude])
