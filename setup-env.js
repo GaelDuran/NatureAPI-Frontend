e022bff8bd906c75325e8d5db9b795cd224f3096
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-12-06 19:50:24 -0600

    Subir docker

--- setup-env.js
+++ setup-env.js
@@ -1,14 +1,27 @@
-const fs = require("fs");
-
-const API_URL = process.env.API_URL || "";
-const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN || "";
-
-const content = `
-export const API_URL = "${API_URL}";
-export const MAPBOX_TOKEN = "${MAPBOX_TOKEN}";
-`;
-
-fs.writeFileSync("src/environments/secret.env.ts", content);
-
-console.log("secret.env.ts generado correctamente");
-
+const path = require('path');
+const { writeFile } = require('fs').promises;
+require('dotenv').config();
+ 
+const requiredVars = ['API_URL', 'MAPBOX_TOKEN'];
+for (const v of requiredVars) {
+  if (!process.env[v]) throw new Error(`${v} debe estar definido en el archivo .env`);
+}
+ 
+const fileName = 'secret.env.ts';
+const filePath = path.join(__dirname, 'src', 'environments', fileName);
+ 
+let fileContent = '';
+for (const v of requiredVars) {
+  fileContent += `export const ${v} = "${process.env[v]}";\n`;
+}
+ 
+async function write() {
+  try {
+    await writeFile(filePath, fileContent, 'utf8');
+    console.log(`${fileName} generado en ${filePath}`);
+  } catch (err) {
+    console.error('Error escribiendo archivo:', err);
+  }
+}
+ 
+write();
